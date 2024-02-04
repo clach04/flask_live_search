9048c33b3c7aa8074fd250528398f295d8cd9f71
clach04 <clach04@gmail.com>
2024-02-04 14:26:01 -0800

    Refactor in prep for fts #3

--- app.py
+++ app.py
@@ -36,17 +36,32 @@ def livesearch():
     #print(request.args.to_dict())
     #print(request.get_json(force=True))
     #print(request.form.to_dict())
-    term = (request.args or {}).get('text') or (request.get_json(force=True) or {}).get('text') or (request.form or {}).get("text")
-    print('term: %r' % term)
+    request_args = request.args or {}
+    request_form = request.form or {}
+    #request_json = request.get_json(force=True) or {}  # how to handle errors, when force true applied
+    request_json = request.get_json() or {}
+    term = (request_args or {}).get('text') or (request.get_json(force=True) or {}).get('text') or (request_form or {}).get("text")
+    print('raw term: %r' % term)
     if term is not None:
         term = term.strip()
         term = term.lower()
+    print('term: %r' % term)
     if not term:
         # fixme error handling - or return everything, probably better option... or option on URL to dictate empty versus full result set
         print('fixme error handling')
         result = []
     else:
-        result = [v for v in live_search_entries if term in v]
+        print('term: %r' % term)
+        fts = request_args.get('fts') or request_form.get("fts")  or request_json.get('fts')
+        print('fts: %r' % fts)
+        if fts is not None:
+            fts = fts.strip()
+            fts = fts.lower()
+        if fts:
+            result = []  # TODO
+        else:
+            # not FTS
+            result = [v for v in live_search_entries if term in v]
     print('Entries returned %r for %r' % (len(result), term))
     #return jsonify(result)  # Microsoft Edge warnings https://github.com/pallets/flask/pull/4752
     resp = flask.Response(response=json.dumps(result),
